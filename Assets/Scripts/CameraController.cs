f4cbec6e2e9d83c9606bd4752759702970061dae
monte0 <47285725+monte0@users.noreply.github.com>
2019-03-09 18:28:05 -0800

    proper camera
    better one

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,13 +1,38 @@
 ﻿using UnityEngine;
 
-public class CameraController : MonoBehaviour
+public class CameraMove : MonoBehaviour
 {
-    public Transform player;
-    public Vector3 offset;
+    private const float Y_ANGLE_MIN = 0.0f;
+    private const float Y_ANGLE_MAX = 50.0f;
+    public Transform lookAt;
+    public Transform camTransform;
 
+    private Camera cam;
+
+    private float distance = 10.0f;
+    private float currentX = 0.0f;
+    private float currentY = 0.0f;
+    private float sensitivityX = 4.0f;
+    private float sensitivityY = 1.0f;
+
+    private void start()
+    {
+        camTransform = transform;
+        cam = Camera.main;
+    }
+
+    private void Update()
+    {
+        currentX += Input.GetAxis("Mouse X");
+        currentY += Input.GetAxis("Mouse Y");
+        currentY = Mathf.Clamp(currentY, Y_ANGLE_MIN, Y_ANGLE_MAX);
+    }
     // Update is called once per frame
     void LateUpdate()
     {
-        transform.position = player.position + offset;
+        Vector3 dir = new Vector3(0, 0, -distance);
+        Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
+        camTransform.position = lookAt.position + rotation * dir;
+        camTransform.LookAt(lookAt.position);
     }
 }
